d68359f3514c8251b53b55b1ef9068f8a5975ef8
Rick Schoenmaker <rickschoenmaker@hotmail.com>
2020-02-19 13:39:05 +0100

    New update 08-02-2020 removed redundant code without a function added try except to catch error

--- DruglikeFiltering.py
+++ DruglikeFiltering.py
@@ -20,7 +20,7 @@ files = []  # list with all the file paths
 
 
 def openfolder():
-    for entry in os.scandir("/home/rick/Documenten/Rick'spdb"): # Give pathway to PDBBind dataset.
+    for entry in os.scandir("/home/rick/Documenten/Testpdb"): # Give pathway to PDBBind dataset.
         if entry.is_dir():
             folders.append(entry.path)  # fill list with folderpaths
 
@@ -58,29 +58,22 @@ def rdkit():
     family = ""
     pos = ""
     smiles = ""
+    list1 = []
 
     for x in sdfFiles:
         suppl = Chem.SDMolSupplier(x)   # Get supplement from SDF File.
-        for mol in suppl:
-            if mol is None: continue
-            lip_HA_count = Lipinski.NumHAcceptors(mol) #set Hydrogen Acceptor count
-            lip_HD_count = Lipinski.NumHDonors(mol)     # Get Hydrogen Donor count
-            lip_LogP_count = Chem.Crippen.MolLogP(mol)  # Get LogP count.
-            lip_MolW_count = Chem.Descriptors.ExactMolWt(mol)   # Get Molecule weight count.
-            feats = []
-            if lip_HA_count < HAcceptor and lip_HD_count < HDonor and lip_LogP_count < LogP and \
-                Mol_Weight_Max > lip_MolW_count > Mol_Weight_Min:   # select ligand on custom cutoff.
-                fdefName = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
-                factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
-                feats = factory.GetFeaturesForMol(mol)
-                smiles = Chem.MolToSmiles(mol)  # Retrieve Smiles from mol.
-                smilesList.append(Chem.MolToSmiles(mol))
-                listRicksPDB.append(x)
-            for f in feats:
-                atom = mol.GetAtomWithIdx(f.GetAtomIds()[0]).GetSymbol()   # Get atoms from molecule.
-                pos = f.GetPos()  # Get X,Y,Z positions from atoms in molecule.
-                family = str(f.GetFamily())  # Get type of atom
-    print(listRicksPDB)
+        try:
+            for mol in suppl:
+                if mol is None: continue
+                lip_HA_count = Lipinski.NumHAcceptors(mol) #set Hydrogen Acceptor count
+                lip_HD_count = Lipinski.NumHDonors(mol)     # Get Hydrogen Donor count
+                lip_LogP_count = Chem.Crippen.MolLogP(mol)  # Get LogP count.
+                lip_MolW_count = Chem.Descriptors.ExactMolWt(mol)   # Get Molecule weight count.
+                if lip_HA_count < HAcceptor and lip_HD_count < HDonor and lip_LogP_count < LogP and \
+                    Mol_Weight_Max > lip_MolW_count > Mol_Weight_Min:   # select ligand on custom cutoff.
+                    listRicksPDB.append(x)
+        except ValueError:
+                pass
     for value in listRicksPDB:
         shutil.move(str(value)[:-15], "/home/rick/Documenten/Rick'spdb")   # Give pathway to directory
     return;
